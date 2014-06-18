1624f4c1860f0b661d6d0944c1aecb346ac3502b
putnampt <putnampt@gmail.com>
2014-06-17 18:00:42 -0700

    Added getChannel

--- neurIO_smr.m
+++ neurIO_smr.m
@@ -1,6 +1,11 @@
 function [ output_args ] = neurIO_smr( input_args )
-%NEURIO_SMR Summary of this function goes here
+%NEURIO_SMR File input/output for
 %   Detailed explanation goes here
+%
+%
+%
+%
+%
 
 fID = fopen(path);
 
@@ -160,3 +165,63 @@ end
 
 end
 
+function [data, header] = getChannel(fID, chan, varargin)
+
+if ischar(fID)==1
+    warning('neurIO_smr: expecting a file handle from fopen(), not a string "%s" on input.',fID );
+    data=[];
+    header=[];
+    return;
+end
+
+
+[path, name, ext]=fileparts(fopen(fID));
+if strcmpi(ext,'.smr') ~=1
+    warning('neurIO_smr: file handle points to "%s". \nThis is not a valid file.',fopen(fID));
+    data=[];
+    header=[];
+    return;
+end
+
+
+channel_info=getInfo_smr(fID,chan);
+if(channel_info.kind==0) 
+    warning('neurIO_smr: Channel #%d does not exist (or has been deleted).',chan);
+    data=[];
+    header=[];
+    return;
+end
+
+switch channel_info.kind
+case {1}
+    [data,header]=getAnalog(fID,chan,varargin{:});
+    [data,header]=convertToDouble(data,header);
+case {2,3,4}
+    [data,header]=getEventChannel(fID,chan,varargin{:});
+case {5}
+    [data,header]=getMarkerChannel(fID,chan,varargin{:});
+case {6}
+    [data,header]=getAnalogMarkerChannel(fID,chan,varargin{:});
+case {7}
+    [data,header]=getRealMarkerChannel(fID,chan,varargin{:});
+case {8}
+    [data,header]=getTextChannel(fID,chan,varargin{:});
+case {9}
+    [data,header]=getRealWaveChannel(fID,chan,varargin{:});
+otherwise
+    warning('neurIO_smr: Channel type not supported.');
+    data=[];
+    header=[];
+    return;
+end
+
+
+switch channel_info.kind
+case {1,6,7,9}
+    if isempty(header)==0
+        header.transpose=0;
+    end
+end
+
+
+end

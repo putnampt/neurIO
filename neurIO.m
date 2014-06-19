97a00f43f062501c7975d737e90008ef335ddbf6
putnampt <putnampt@gmail.com>
2014-06-18 17:08:47 -0700

    NeurIO master function parser updated

--- neurIO.m
+++ neurIO.m
@@ -1,25 +1,21 @@
-function [ varargout ] = neurIO( filename, varargin )
+function [ varargout ] = neurIO( varargin )
 %NEURIO File input/output from binary neurophysiology data formats
 %   Philip Putnam, 2014.
 %   Version 0.1.1
 %   This function serves as a wrapper, parsing input and utilizing the
 %   format specific functions for actual file reading and writing.
 
-%%  Parse the input
+%%  Hard coded varibles
+validFormats = {'.smr';'.plx';'.pl2'}; % List of valid formats for input files
+
+%%  If no input parameters are specified, use pop-up to locate data file
+if size(varargin) == [0,0]
+     [FileName,PathName,FilterIndex] = uigetfile('*.*');
+    varargin{1} = fullfile(PathName, FileName);
+end
+
+%%  Parse the input parameters
 p = inputParser; % Create input parser objection
-
-
-[pathstr,name,ext] = fileparts(filename); % Split filename input
-validFormats = {'.smr','.plx', '.pl2'}; % List of valid formats for input files
-%checkFormat = @(x) any(validatestring(x,validFormats)); % Function to check if is a valid formats
-any(validatestring(ext,validFormats))
-
-defaultMode = 'pro'; % Default mode is programmatic 
-validModes = {'pro','gui', 'pop'}; % Options are programmatic, GUI, or pop-up windows
-checkMode = @(x) any(validatestring(x,validModes)); % Function to check if is a valid mode
-
-checkFile = @(x) exist(x, 'file'); % Function to check input file exists
-
 
 %   Default values
 defaultStart = -1; % Time in miliseconds to start extraction, value of -1 is first time point
@@ -27,39 +23,54 @@ defaultStop = -1; % Time in miliseconds to end extract, value of -1 is last time
 defaultResample = -1;
 defaultChannels = [];
 
-%   Set Parser 
+checkFile = @(x) exist(x, 'file'); % Function to check input file exists
+
+%   Set Parser
 addRequired(p,'filename',checkFile);
-addOptional(p,'mode',defaultMode,checkMode);
 addParameter(p,'start',defaultStart,@isnumeric);
 addParameter(p,'stop',defaultStop,@isnumeric);
 addParameter(p,'resample',defaultResample,@isnumeric);
 addParameter(p,'channels',defaultChannels);
-
 p.KeepUnmatched = true; % Don't worry about case-matching for inputs
 
-parse(p,filename,varargin{:}); % Parse the input
+parse(p,varargin{:}); % Parse the input
 
-disp(['File name: ',p.Results.filename])
-disp(['Mode: ', p.Results.mode])
+fprintf('File name: %s\n\n',p.Results.filename)
 
 if ~isempty(fieldnames(p.Unmatched))
-   disp('Extra inputs:')
-   disp(p.Unmatched)
+    fprintf('Extra inputs:')
+    disp(p.Unmatched)
 end
 if ~isempty(p.UsingDefaults)
-   disp('Using defaults: ')
-   disp(p.UsingDefaults)
+    fprintf('Using default: ')
+    disp(p.UsingDefaults)
+end
+
+[pathstr,name,ext] = fileparts(p.Results.filename); % Split filename input
+any(validatestring(ext,validFormats)); % Check to see if it's a valid format
+
+options =  p.Results; % Assign the matched parameters to a stucture
+
+switch ext
+    case '.smr' 
+        fprintf('Opening %s as .SMR file.\n', p.Results.filename)
+        neurIO_smr()
+    case {'.plx','.pl2'}
+        fprintf('Opening %s as .PLX file.\n', p.Results.filename)
+    otherwise
+        warning('Unexpected format.');
+end
+
+
 end
 
 
 
-%%
-end
 
-function [file] = neurIO_resample(file, fs)
-%NEURIO_RESAMPLE Resamples all channels to a specified frequency (hertz)
-end
-
-function neurIO_writeFile(file, path)
-%NEURIO_WRITEFILE Writes file output to disk
-end
+% function [file] = neurIO_resample(file, fs)
+% %NEURIO_RESAMPLE Resamples all channels to a specified frequency (hertz)
+% end
+% 
+% function neurIO_writeFile(file, path)
+% %NEURIO_WRITEFILE Writes file output to disk
+% end
